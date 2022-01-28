2af48c7e692f1d52a3144c0ed22f995d15ebf2cb
HonCich <47360906+HonCich@users.noreply.github.com>
2022-01-28 20:51:39 +0100

    motor problem

--- software/filament_recycler/src/main.cpp
+++ software/filament_recycler/src/main.cpp
@@ -1,24 +1,31 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
-#include "AccelStepper.h"
 #include "Wire.h"
 #include "PCA9685.h"
 #include "HX711.h"
+#include "AccelStepper.h"
+#include "MultiStepper.h"
+
+
+const uint8_t reflectivesensor0 = 34;
+const uint8_t PCA_OE = 25;
+const uint8_t temp0 = 32;
+const uint8_t temp1 = 33;
+const uint8_t step0 = 12;
+const uint8_t dir0 = 5;
+const uint8_t step1 = 17;
+const uint8_t dir1 = 0;
+
+const float c1 = 0.0007157730988481782;
+const float c2 = 0.0002145860245372531;
+const float c3 = 1.0991444718982721e-7;
 
 PCA9685 PCA(0x70);
-
-static uint8_t reflectivesensor0 = 34;
-static uint8_t PCA_OE = 25;
-static uint8_t temp0 = 32;
-static uint8_t temp1 = 33;
-
-static float c1 = 0.0007157730988481782;
-static float c2 = 0.0002145860245372531;
-static float c3 = 1.0991444718982721e-7;
+AccelStepper stepper0(AccelStepper::DRIVER, step0, dir0);
 
 void outputEnable (bool enable);
-void fiveVDrive (char output, bool value);
-void twentyfourVDrive (char output, bool value);
+void fiveVDrive (char output, int value);
+void twentyfourVDrive (char output, int value);
 int reflectiveRead ();
 float tempRead (char temp);
 
@@ -38,7 +45,7 @@ void outputEnable (bool enable)
   }
 }
 
-void fiveVDrive (char output, bool value)
+void fiveVDrive (char output, int value)
 {
   char pin;
   switch(output)
@@ -62,15 +69,7 @@ void fiveVDrive (char output, bool value)
      pin = 1;
       break;
   }
-  if(value==1)
-  {
-    PCA.setON(pin);
-  }
-  else
-  {
-    PCA.setOFF(pin);
-  }
-  PCA.setPWM(pin,4095-value*40.95,value*40.95);
+  PCA.setPWM(pin,0+value*40.95);
 }
 
 void twentyfourVDrive (char output, int value)
@@ -97,7 +96,7 @@ void twentyfourVDrive (char output, int value)
       pin = 7;
       break;
   }
-  PCA.setPWM(pin,4095-value*40.95,value*40.95);
+  PCA.setPWM(pin,0+value*40.95);
 }
 
 int reflectiveRead ()
@@ -114,27 +113,37 @@ float tempRead (char temp)
   switch(temp)
   {
     case 0:
-      R = 100000*(1023/analogRead(temp0)-1);
+       R = 100000 / (4095.0 / analogRead(temp0) - 1.0);
       break;
     case 1:
-      R = 100000*(1023/analogRead(temp1)-1);
+      R = 100000 / (4095.0 / analogRead(temp1) - 1.0);
       break;
   }
   logR = log(R);
-  T = (1/(c1+c2*logR+c3*logR*logR*logR));
+  T = (1 / (c1 + c2 * logR + c3 * logR * logR * logR)) - 273.15;
+  Serial.println(T);
   return(T);
 }
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  //Serial.begin(115200);
   Wire.begin(); 
   PCA.begin();
+
   pinMode (PCA_OE, OUTPUT);
   outputEnable(1);
+  stepper0.setAcceleration(200.0);
+  stepper0.setSpeed(100);
+  stepper0.move(1000);
 }
+
+
+int x = 0;
 
 void loop() {
   // put your main code here, to run repeatedly
-  
+    
+    stepper0.run();
+
 }
